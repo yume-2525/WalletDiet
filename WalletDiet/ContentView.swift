1ff2ba2bd78d04af88d38f3669b19ef73ae32748
asa05z2 <a23ninitkd@gmail.com>
2024-09-02 18:10:49 +0900

    Initial Commit

---
+++ WalletDiet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WalletDiet
+//
+//  Created by 高田梓沙 on 2024/09/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WalletDiet/WalletDietApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletDietApp.swift
+//  WalletDiet
+//
+//  Created by 高田梓沙 on 2024/09/02.
+//
+
+import SwiftUI
+
+@main
+struct WalletDietApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
